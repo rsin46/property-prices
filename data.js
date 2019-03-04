392c62c155bbf1e84ff9b21fd08379a177dcb7bc
rsin46 <46255581+rsin46@users.noreply.github.com>
2019-03-04 20:53:05 +1100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+// var file_topo = 'syd_s.topojson'
+// var co_ordinates = [-33.842984,151.135761];
+// var min_yield = 0.001;
+// var max_yield = 0.062;
+// var min_price = 100000;
+// var max_price = 1000000;
+// var min_rent = 100;
+// var max_rent = 750;
+
+var file_topo = 'melb_s.topojson'
+var co_ordinates = [-37.90403,144.986975];
+var min_yield = 0.001;
+var max_yield = 0.12;
+var min_price = 100000;
+var max_price = 700000;
+var min_rent = 100;
+var max_rent = 440;
